2fb310b157857f32f626066e3a85bb9e16ae9353
Valerie Dryden <outragedpinkracoon@gmail.com>
2019-12-07 17:23:27 +0000

    Wee test pass lol

--- day_6/lib/day6.ex
+++ day_6/lib/day6.ex
@@ -2,8 +2,14 @@ defmodule Day6 do
   @moduledoc """
   Documentation for Day6.
   """
+  def run(result \\ %{}, _)
 
-  def hello do
-    :world
+  def run(result, []), do: result
+  def run(result, [h | t]) do
+    update(h, result) |> run(t)
+  end
+
+  def update({orbitted, orbiter}, result) do
+    Map.put(result, orbiter, orbitted)
   end
 end
--- day_6/test/day6_test.exs
+++ day_6/test/day6_test.exs
@@ -2,7 +2,24 @@ defmodule Day6Test do
   use ExUnit.Case
   doctest Day6
 
-  test "greets the world" do
-    assert Day6.hello() == :world
+  test "sample 1" do
+    assert Day6.run([{"COM", "B"}]) == %{"B" => "COM"}
   end
+
+  # test "sample 2" do
+  #   input = [
+  #     {COM, B},
+  #     {B, C},
+  #     {B, G},
+  #     {G, H}
+  #   ]
+
+  #   output = %{
+  #     "B": ["COM"],
+  #     "C": ["B"],
+  #     "G": ["B"],
+  #     "H": ["G"]
+  #   }
+  #   assert Day6.run(input) == output
+  # end
 end
